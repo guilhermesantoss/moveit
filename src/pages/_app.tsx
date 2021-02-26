8a227cf632243bf133e5d03bf82306ce61934442
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2021-02-26 10:24:14 -0300

    add cookies to persist customer data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/global.css';
 import '../styles/media.css';
 
-import { ChallengesProvider } from '../context/ChallengesContext';
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
