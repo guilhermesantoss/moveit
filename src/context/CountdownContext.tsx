4d1ffc10bfc630d22ed812e522a3f2e4bba17f83
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2021-02-27 18:38:01 -0300

    add responsive

--- src/context/CountdownContext.tsx
+++ src/context/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(30 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -35,7 +35,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(30 * 60);
+    setTime(0.1 * 60);
     setHasFinished(false);
   }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/global.css';
-import '../styles/media.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
